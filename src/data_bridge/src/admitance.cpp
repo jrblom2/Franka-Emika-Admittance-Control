5acb3cba1a51bb96d21a0e879646b54284ee3143
Joseph Blom <jblom5400@gmail.com>
2025-05-21 13:54:38 -0500

    much better, too little stiffness is unstable

--- src/data_bridge/src/admitance.cpp
+++ src/data_bridge/src/admitance.cpp
@@ -74,7 +74,7 @@ int main(int argc, char** argv) {
   std::string calc_mode{argv[2]};
 
   // Compliance parameters
-  const double translational_stiffness{10.0};
+  const double translational_stiffness{20.0};
   const double rotational_stiffness{50.0};
   Eigen::MatrixXd stiffness(6, 6), damping(6, 6);
   stiffness.setZero();
