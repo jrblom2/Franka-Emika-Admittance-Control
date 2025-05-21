6971683296ad7cbf0f32eb1a9ba350f3e3a50490
Joseph Blom <jblom5400@gmail.com>
2025-05-21 10:14:37 -0500

    update rsync script to also ignore colcon directories

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-rsync -a --no-owner --no-group --progress --exclude 'build/' /home/joe/final/TheWiggler/ student@station:/home/student/TheWiggler
+rsync -a --no-owner --no-group --progress \
+  --exclude 'build/' \
+  --exclude 'install/' \
+  --exclude 'log/' \
+  /home/joe/final/TheWiggler/ \
+  student@station:/home/student/TheWiggler
