2cde335d74a464d62534c855b7861694d6be90e0
Joseph Blom <jblom5400@gmail.com>
2025-05-28 13:23:39 -0500

    add damping paramaters and lower translational damping

--- src/data_bridge/src/admitance.cpp
+++ src/data_bridge/src/admitance.cpp
@@ -76,14 +76,16 @@ int main(int argc, char** argv) {
   // Compliance parameters
   const double translational_stiffness{100.0};
   const double rotational_stiffness{35.0};
+  const double translational_damping_factor{1.5};
+  const double rotational_damping_factor{2.0};
   Eigen::MatrixXd stiffness(6, 6), damping(6, 6);
   stiffness.setZero();
   stiffness.topLeftCorner(3, 3) << translational_stiffness * Eigen::MatrixXd::Identity(3, 3);
   stiffness.bottomRightCorner(3, 3) << rotational_stiffness * Eigen::MatrixXd::Identity(3, 3);
   damping.setZero();
-  damping.topLeftCorner(3, 3) << 2.0 * sqrt(translational_stiffness) *
+  damping.topLeftCorner(3, 3) << translational_damping_factor * sqrt(translational_stiffness) *
                                      Eigen::MatrixXd::Identity(3, 3);
-  damping.bottomRightCorner(3, 3) << 2.0 * sqrt(rotational_stiffness) *
+  damping.bottomRightCorner(3, 3) << rotational_damping_factor * sqrt(rotational_stiffness) *
                                          Eigen::MatrixXd::Identity(3, 3);
 
   //connect to sensor
