18b7d2d8f4608d56905aecacc128a8794f002067
Joseph Blom <jblom5400@gmail.com>
2025-05-29 13:48:44 -0500

    working position plotting

--- src/data_analysis/data_analysis/position_tracking.py
+++ src/data_analysis/data_analysis/position_tracking.py
@@ -13,26 +13,59 @@ class MinimalSubscriber(Node):
         self.subscription = self.create_subscription(Robot, 'robot_data', self.listener_callback, 10)
         self.subscription  # prevent unused variable warning
         self.timer = self.create_timer(1, self.timer_callback)
-        self.xForce = []
-        self.axis = []
+        self.xPosition = []
+        self.yPosition = []
+        self.zPosition = []
+        self.time = []
 
         plt.ion()
-        self.fig, self.ax = plt.subplots()
-        (self.line,) = self.ax.plot([], [], 'r-')
-        self.ax.set_title("Force in X direction")
-        self.ax.set_xlabel("Time")
-        self.ax.set_ylabel("Force")
+        self.fig, self.axes = plt.subplots(2, 3, figsize=(10, 8))
+        self.lines = []
+
+        ax = self.axes[0, 0]
+        (lineX,) = ax.plot([], [], 'r-')
+        ax.set_title("X Position")
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Position")
+        self.lines.append(lineX)
+
+        ax = self.axes[0, 1]
+        (lineY,) = ax.plot([], [], 'g-')
+        ax.set_title("Y Position")
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Position")
+        self.lines.append(lineY)
+
+        ax = self.axes[0, 2]
+        (lineZ,) = ax.plot([], [], 'b-')
+        ax.set_title("Z Position")
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Position")
+        self.lines.append(lineZ)
+
         self.counter = 0
 
     def listener_callback(self, msg):
-        self.xForce.append(msg.wrench.force.x)
-        self.axis.append(self.counter / 20)
+        self.xPosition.append(msg.position.position.x)
+        self.yPosition.append(msg.position.position.y)
+        self.zPosition.append(msg.position.position.z)
+        self.time.append(self.counter / 20)
         self.counter += 1
 
     def timer_callback(self):
-        self.line.set_data(self.axis, self.xForce)
-        self.ax.relim()
-        self.ax.autoscale_view()
+        self.lines[0].set_data(self.time, self.xPosition)
+        self.axes[0, 0].relim()
+        self.axes[0, 0].autoscale_view()
+
+        self.lines[1].set_data(self.time, self.yPosition)
+        self.axes[0, 1].relim()
+        self.axes[0, 1].autoscale_view()
+
+        self.lines[2].set_data(self.time, self.zPosition)
+        self.axes[0, 2].relim()
+        self.axes[0, 2].autoscale_view()
+
+        self.fig.tight_layout()
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
         plt.pause(0.1)
