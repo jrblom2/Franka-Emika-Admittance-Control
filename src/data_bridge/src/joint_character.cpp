82e054b9d83f5afbdf8252c56fae66160ae1991b
Joseph Blom <jblom5400@gmail.com>
2025-06-26 12:49:48 -0500

    add some initial results and small test example for use in characterizing joints

---
+++ src/data_bridge/src/joint_character.cpp
@@ -0,0 +1,194 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+#include <array>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <csignal>
+
+#include <eigen3/Eigen/Dense>
+
+#include <franka/duration.h>
+#include <franka/exception.h>
+#include <franka/model.h>
+#include <franka/robot.h>
+
+#include "examples_common.h"
+#include "net_ft/hardware_interface.hpp"
+#include "SafeQueue.hpp"
+#include "spring_simulate.hpp"
+#include "minimal_publisher.hpp"
+#include "data_dumper.hpp"
+
+volatile bool robot_stop = false; // Global flag
+
+void signal_handler(int signal) {
+    if (signal == SIGINT) {
+        std::cerr << "\nCtrl+C detected. Initiating graceful shutdown..." << std::endl;
+        robot_stop = true;
+    }
+}
+
+/**
+ * @example cartesian_impedance_control.cpp
+ * An example showing a simple cartesian impedance controller without inertia shaping
+ * that renders a spring damper system where the equilibrium is the initial configuration.
+ * After starting the controller try to push the robot around and try different stiffness levels.
+ *
+ * @warning collision thresholds are set to high values. Make sure you have the user stop at hand!
+ */
+int main(int argc, char** argv) {
+  std::signal(SIGINT, signal_handler);
+  // Check whether the required arguments were passed
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << " <publish?>" << std::endl;
+    return -1;
+  }
+
+  std::string ros2_publish{argv[2]};
+
+  // thread-safe queue to transfer robot data to ROS
+  std::thread spin_thread;
+  SafeQueue<queue_package> transfer_package;
+  std::vector<queue_package> dump_vector;
+
+  try {
+    // connect to robot
+    franka::Robot robot(argv[1]);
+    setDefaultBehavior(robot);
+
+    // First move the robot to a suitable joint configuration
+    std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+    MotionGenerator motion_generator(0.5, q_goal);
+
+    robot.control(motion_generator);
+    std::cout << "Finished moving to initial joint configuration." << std::endl;
+
+    // load the kinematics and dynamics model
+    franka::Model model = robot.loadModel();
+
+    franka::RobotState initial_state = robot.readOnce();
+
+    // equilibrium point is the initial position
+    Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
+    Eigen::Vector3d position_d(initial_transform.translation());
+    Eigen::Quaterniond orientation_d(initial_transform.rotation());
+
+    // set collision behavior
+    robot.setCollisionBehavior({{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
+                               {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
+                               {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}},
+                               {{100.0, 100.0, 100.0, 100.0, 100.0, 100.0}});
+    
+    // define callback for the torque control loop
+    std::function<franka::Torques(const franka::RobotState&, franka::Duration)>
+        impedance_control_callback = [&](const franka::RobotState& robot_state,
+                                         franka::Duration /*duration*/) -> franka::Torques {
+      // get state variables
+      std::array<double, 7> coriolis_array = model.coriolis(robot_state);
+      std::array<double, 7> gravity_array = model.gravity(robot_state);
+      std::array<double, 42> jacobian_array =
+          model.zeroJacobian(franka::Frame::kEndEffector, robot_state);
+      std::array<double, 49> mass_array = model.mass(robot_state);
+
+      // convert to Eigen
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> gravity(gravity_array.data());
+      Eigen::Map<const Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 7>> mass(mass_array.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> tau_J(robot_state.tau_J.data());
+      Eigen::Map<const Eigen::Matrix<double, 7, 1>> tau_J_d(robot_state.tau_J_d.data());
+      Eigen::Matrix<double, 6, 1> fext;
+      Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
+      Eigen::Vector3d position(transform.translation());
+      Eigen::Quaterniond orientation(transform.rotation());
+      
+      // compute control
+      Eigen::VectorXd tau_task(7), tau_d(7);
+      static int fullCount = 0;
+      fullCount++;
+
+      tau_task.setZero();
+      double goal = sin(fullCount * 2 * M_PI / 5000)
+      tau_task(0) = 1.0 * goal;
+
+      // add all control elements together
+      tau_d << tau_task + coriolis;
+
+      // output format
+      std::array<double, 7> tau_d_array;
+      Eigen::Map<Eigen::Matrix<double, 7, 1>>(tau_d_array.data()) = tau_d;
+      franka::Torques torques = tau_d_array;
+
+      // publish results
+      static int count = 0;
+      count++;
+      static Eigen::Vector3d predicted = position;
+
+      if (count == 10) {
+        queue_package new_package;
+        new_package.desired_accel = Eigen::Matrix<double, 6, 1>::Zero();
+        new_package.actual_wrench = Eigen::Matrix<double, 6, 1>(fext);
+        new_package.orientation_error = Eigen::Matrix<double, 3, 1>::Zero();
+        new_package.translation = Eigen::Vector3d(position);
+        new_package.translation_d = Eigen::Vector3d(predicted);
+        new_package.velocity = (jacobian * dq).head(3).reshaped();
+        new_package.torques_d = tau_task;
+        new_package.torques_o = tau_J_d.reshaped();
+        new_package.torques_c = coriolis.reshaped();
+        new_package.torques_g = tau_J.reshaped() - gravity.reshaped();
+        new_package.ddq_d = Eigen::VectorXd(7);
+        if (ros2_publish == "TRUE") {
+          transfer_package.Produce(std::move(new_package));
+        }
+        dump_vector.push_back(std::move(new_package));
+        count = 0;
+      }
+
+      // if ctrl-c is pressed, robot should stop
+      if (robot_stop) {
+        return franka::MotionFinished(torques);
+      }
+      return torques;
+    };
+
+    // start real-time control loop
+    std::cout << "WARNING: Collision thresholds are set to high values. "
+              << "Make sure you have the user stop at hand!" << std::endl
+              << "After starting try to push the robot and see how it reacts." << std::endl
+              << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
+
+    // data bridge through ROS2 setup
+    if (ros2_publish == "TRUE") {
+      rclcpp::init(argc, argv);
+      auto node = std::make_shared<MinimalPublisher>(transfer_package);
+      rclcpp::executors::MultiThreadedExecutor executor;
+      executor.add_node(node);
+      spin_thread = std::thread([&executor]() { executor.spin(); });
+    }
+
+    robot.control(impedance_control_callback);
+  } catch (const franka::Exception& ex) {
+    // print exception
+    std::cout << ex.what() << std::endl;
+  } catch (const std::exception& ex) {
+    std::cerr << ex.what() << std::endl;
+  } catch (...) {
+      std::cerr << "Unknown exception caught." << std::endl;
+  }
+
+  if (ros2_publish == "TRUE") {
+    rclcpp::shutdown();
+    spin_thread.join();
+  }
+
+  robot_dump(dump_vector);
+
+  return 0;
+}
